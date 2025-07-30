3cbdd099fab899a6cde7cb7c71351a9e17c3f17e
iwurui <59505004+iwurui@users.noreply.github.com>
2025-07-30 20:16:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='qnapdisplay',
       version='0.1',
       description='Library for interacting with front-panel displays on Qnap products',
-      url='http://github.com/bkram/qnapdisplay',
+      url='http://github.com/iwurui/qnapdisplay',
       author='Mark de Bruijn',
       license='GPL2',
       packages=['qnapdisplay'],
